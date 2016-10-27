0238e459a8802a4636180c3c25527e64454254ef
codebooker <codebooker@gmail.com>
2016-10-27 00:44:13 -0400

    Added Delay To Preloader Vanishing (1000ms)

--- js/preloader.js
+++ js/preloader.js
@@ -22,6 +22,8 @@ tl.add(
 var overlay = document.getElementById("overlay");
 
 window.addEventListener('load', function(){
+  setTimeout(function() {
   overlay.style.display = 'none';
+}, 1000);
 });
 // END OF PRELOADER STUFF
