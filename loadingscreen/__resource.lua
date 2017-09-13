b72c57a09612c2660cd44e3b6818f2184f3a7c53
Jack T <codebooker@gmail.com>
2017-09-13 19:07:04 -0400

    Loadingscreen

---
+++ loadingscreen/__resource.lua
@@ -0,0 +1,16 @@
+files {
+    'index.html',
+    'css/normalize.css',
+    'css/style.css',
+    'images/divider.png',
+    'images/pattern.png',
+    'css/Wisdom.otf',
+    'js/custom.js',
+    'js/device.min.js',
+    'js/jquery.mb.YTPlayer.js',
+    'js/jquery-1.11.1.min.js',
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '72431fab-63ca-442c-c67b-abc70f28dfa5'
