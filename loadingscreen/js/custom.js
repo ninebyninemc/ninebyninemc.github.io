b87c57074fd3005a3147400ad60da1d7528aa7f2
Jack T <codebooker@users.noreply.github.com>
2019-08-18 04:05:59 -0400

    sdf

--- loadingscreen/js/custom.js
+++ loadingscreen/js/custom.js
@@ -32,3 +32,34 @@
 
 
 })(jQuery);
+
+var tag = document.createElement("script");
+
+tag.src = "//www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player("ytplayer", {
+    events: {
+      onReady: onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady() {
+  player.playVideo();
+  // Mute Sound Function!
+  player.mute();
+  setTimeout(endOfVideo,90000);
+}
+
+function endOfVideo() {
+    var myElement = document.querySelector(".videooverlay");
+    myElement.style.opacity = "1";
+    (function fade(){(myElement.style.opacity-=.1)<0?myElement.style.display="none":setTimeout(fade,40)})();
+}
+
+
